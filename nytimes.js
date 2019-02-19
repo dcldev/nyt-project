f6b5743432ca5e67fb4af9a0b7f554f482d30c77
Han Seoulo <cu.rogue.coder@gmail.com>
2019-02-19 04:15:13 -0500

    Danny Edits 1 cd ; / 1 git commit cd . exit clear $ m

--- nytimes.js
+++ nytimes.js
@@ -15,6 +15,7 @@ $.ajax({
 
         console.log(response);
 
+        
         var results = response.response.docs;
 
         // map method allows for deeper access to multiple arrays within objects
@@ -23,12 +24,38 @@ $.ajax({
         // })
 
         for (var i=0; i < results.length; i++) {
-        
-        var returnArticle = $("<div>");
+            var articleTitle = results[i].headline.main;
+            var articleSnippet = results[i].snippet;
+            var articleAuth = results[i].byline.original
+            var articleUrl = results[i].web_url;
 
-        // console.log(returnArticle);
+            $(".articleLister").append(`
+                <div class="card mb-3" style="max-width: 760px;">
+                <div class="row no-gutters">
+                  <div class="col-md-4">
+                    <img src="t_logo_291_black.png" class="card-img" alt="nytlogo">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">${articleTitle}</h5>
+                        <p class="card-snip">${articleSnippet}</p>
+                        <p class="card-auth">${articleAuth}</p>
+                        <a href="${articleUrl}">Click Here for Article</a>
+                    </div>
+                  </div>
+                </div>
+              </div>`
+                );
 
-        $("#topArticles").prepend(results[i].web_url)
+            
+
+            // $(".card-title").html(articleTitle)
+            // $(".card-snip").html(articleSnippet)
+            // $(".card-auth").html(articleAuth)
+            // $("a").attr("href", articleUrl)
+
+            
         };
     });
 });
+
